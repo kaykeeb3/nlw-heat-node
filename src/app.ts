b14a5acd87aaf10698f6aee30bca5022e0e19f5f
Kayke-Ti <kaykeloiola@gmail.com>
2024-01-22 12:15:34 -0300

    feat: login route (github) & callback route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,19 @@
-import express from "express";
+import "dotenv/config";
+import express, { request, response } from "express";
 
 const app = express();
+
+app.get("/github", (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
 
 app.listen(4000, () => {
   console.log(`🚀 Server is  running on PORT 4000`);
