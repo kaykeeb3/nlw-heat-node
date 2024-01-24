f73569928f8004259453b1567779195c18005eaa
Kayke-Ti <kaykeloiola@gmail.com>
2024-01-24 08:49:18 -0300

    feat: configuring websocket & new server port

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import "dotenv/config";
-import express, { request, response } from "express";
+import express from "express";
+import cors from "cors";
+import http from "node:http";
+import { Server, Socket } from "socket.io";
 
 import { router } from "./routes";
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
 app.use(express.json());
 
 app.use(router);
@@ -20,6 +37,4 @@ app.get("/signin/callback", (request, response) => {
   return response.json(code);
 });
 
-app.listen(4000, () => {
-  console.log(`🚀 Server is  running on PORT 4000`);
-});
+export { serverHttp, io };
--- src/controller/CreateMessageController.ts
+++ src/controller/CreateMessageController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { AuthenticateUserService } from "../services/AuthenticateUserService";
 import { CreateMessageService } from "../services/CreateMessageService";
 
 class CreatemessageController {
