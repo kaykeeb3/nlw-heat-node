29e562dc4330977af7475e94778e995250db7eee
Kayke-Ti <kaykeloiola@gmail.com>
2024-01-24 09:07:03 -0300

    feat: creating 3 latest messages & creating user profile

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
 import { CreatemessageController } from "./controller/CreateMessageController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+import { Get3LastMessagesController } from "./controller/GetLast3MessagesController";
+import { ProfileUserController } from "./controller/ProfileUserController";
 
 const router = Router();
 
@@ -12,5 +14,9 @@ router.post(
   ensureAuthenticated,
   new CreatemessageController().handle
 );
+
+router.get("/messages/last3", new Get3LastMessagesController().handle);
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };
