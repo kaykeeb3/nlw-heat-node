84de948da6621a80ae75902e8161a4011bdd9f21
Kayke-Ti <kaykeloiola@gmail.com>
2024-01-24 08:23:44 -0300

    feat: creating the registration of messages & (add new @types in express - typescript)

---
+++ src/controller/CreateMessageController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { AuthenticateUserService } from "../services/AuthenticateUserService";
+import { CreateMessageService } from "../services/CreateMessageService";
+
+class CreatemessageController {
+  async handle(request: Request, response: Response) {
+    const { message } = request.body;
+    const { user_id } = request;
+
+    const service = new CreateMessageService();
+
+    const result = await service.execute(message, user_id);
+
+    return response.json(result);
+  }
+}
+
+export { CreatemessageController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
+import { CreatemessageController } from "./controller/CreateMessageController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreatemessageController().handle
+);
 
 export { router };
